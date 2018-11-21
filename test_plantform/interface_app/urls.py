f8ec0f9c5464f8c03e7c2e4efc45a5add70759e8
sunflowerLan <317229052@qq.com>
2018-11-21 12:06:53 +0800

    添加结果验证，实现修改用例功能

--- test_plantform/interface_app/urls.py
+++ test_plantform/interface_app/urls.py
@@ -4,10 +4,12 @@ from interface_app import views
 urlpatterns = [
     # 项目
     path('case_manage/', views.case_manage, name='case_manage'),
-    path('api_debug/', views.api_debug, name='api_debug'),
-    path('debug/', views.debug),
+    path('add_case/', views.add_case, name='api_debug'),
+    path('api_debug/', views.debug),
     path('save_case/', views.save_case),
     path('edit_case/<int:case_id>/', views.edit_case, name='edit'),
     path('delete_case/<int:case_id>/', views.delete_case, name='delete'),
     path('search_case/', views.search_case, name="search"),
+    path('get_case_info/', views.get_case_info, name="getCaseInfo"),
+    path('api_assert/', views.api_assert),
 ]
--- test_plantform/interface_app/views.py
+++ test_plantform/interface_app/views.py
@@ -2,7 +2,7 @@ import re
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.db.models import Q
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
 from project_app.models import Project
 from project_app.models import Module
@@ -11,31 +11,53 @@ from .forms import TestCaseForm
 import requests
 import json
 
-# Create your views here.
-@login_required
-def case_manage(request):
-    # 读取session
-    username = request.session.get('u', None)
-    testcase = TestCase.objects.all()
-    # 分页
-    paginator = Paginator(testcase, 3)
-    page = request.GET.get('page')
+def pageInation(query_obj, default_item, get_page):
+    """
+    分页功能实现
+    :param query_obj:
+    :param default_item:
+    :param get_page:
+    :return:
+    """
+    # 默认每页显示default_item条
+    paginator = Paginator(query_obj, default_item)
     try:
-        contacts = paginator.page(page)
+        # 获取第get_page页
+        contacts = paginator.page(get_page)
     except PageNotAnInteger:
         # 如果页数不是整型, 取第一页.
         contacts = paginator.page(1)
     except EmptyPage:
         # 如果页数超出查询范围，取最后一页
         contacts = paginator.page(paginator.num_pages)
+    # 返回分页结果
+    return contacts
 
+# Create your views here.
+@login_required
+def case_manage(request):
+    """
+    测试用例管理
+    :param request:
+    :return:
+    """
+    # 读取session
+    username = request.session.get('u', None)
+    testcase = TestCase.objects.all().order_by('-create_time')
+    # 分页
+    page = request.GET.get('page')
+    contacts = pageInation(testcase, 5, page)
     if request.method == "GET":
         return render(request, 'interface_app/case_manage.html', {'user': username, 'type': 'list', 'testcases': contacts})
     else:
         return HttpResponse("404")
 
-# 查询
 def search_case(request):
+    """
+    查询测试用例功能实现
+    :param request:
+    :return:
+    """
     if request.method == "POST":
         case_name = request.POST.get("case_name")
         case_module = request.POST.get("case_module")
@@ -43,37 +65,39 @@ def search_case(request):
 
         # 判断查询条件是否为空
         if case_name.strip() == "":
-            testcase = TestCase.objects.all()
+            testcase = TestCase.objects.all().order_by('-create_time')
         else:
-            testcase = TestCase.objects.filter(Q(name__icontains=case_name))
+            testcase = TestCase.objects.filter(Q(name__icontains=case_name)).order_by('-create_time')
 
+        # print("查询结果为：", testcase)
         # 分页
-        paginator = Paginator(testcase, 3)
         page = request.GET.get('page')
-        try:
-            contacts = paginator.page(page)
-        except PageNotAnInteger:
-            # 如果页数不是整型, 取第一页.
-            contacts = paginator.page(1)
-        except EmptyPage:
-            # 如果页数超出查询范围，取最后一页
-            contacts = paginator.page(paginator.num_pages)
-            return render(request, 'interface_app/case_manage.html',
+        contacts = pageInation(testcase, 5, page)
+        # 返回查询结果
+        return render(request, 'interface_app/case_manage.html',
                           {'type': 'list', 'testcases': contacts, 'case_name': case_name, 'module': case_module})
     else:
         return HttpResponse("404")
 
-# 进入调试接口页面
-def api_debug(request):
+def add_case(request):
+    """
+    打开新增接口页面
+    :param request:
+    :return:
+    """
     if request.method == "GET":
         project_list = Project.objects.all()
-        return render(request,'interface_app/case_manage.html', {'type': 'debug', 'project_list': project_list})
+        return render(request,'interface_app/case_manage.html', {'type': 'add', 'project_list': project_list})
     else:
         return HttpResponse("404")
 
-# 调试接口
 @login_required
 def debug(request):
+    """
+    调试接口
+    :param request:
+    :return:
+    """
     # print("request method:" + request.method)
     if request.method == "GET":
         req_url = request.GET.get("url")
@@ -108,10 +132,16 @@ def debug(request):
     else:
         return HttpResponse("请求方法错误")
 
-# 保存用例
+
 @login_required
 def save_case(request):
+    """
+    保存用例
+    :param request:
+    :return:
+    """
     if request.method == "POST":
+        case_id = request.POST.get("case_id")
         req_name = request.POST.get("name")
         req_url = request.POST.get("url")
         req_method = request.POST.get("method")
@@ -119,6 +149,7 @@ def save_case(request):
         req_parameter = request.POST.get("parameter")
         req_headers = request.POST.get("header")
         req_module = request.POST.get("module_id")
+        req_assert = request.POST.get("assert")
 
         if req_url == "" or req_method == "" or req_type == "" or req_module == "":
             return HttpResponse("必传参数为空")
@@ -128,59 +159,64 @@ def save_case(request):
             req_headers = "{}"
         if url_validate(req_url):
             module_obj = Module.objects.get(id=req_module)
-            # print(module_obj)
-            testcase = TestCase.objects.create(name=req_name, module=module_obj, url=req_url,
-                                       req_method=req_method, req_header=req_headers,
-                                       req_type=req_type, req_parameter=req_parameter)
-            if testcase is not None:
-                return HttpResponse("保存成功！")
+            # 如果case_id非空，则更新用例；如果为空，就创建用例
+            if case_id:
+                # 查找编辑的用例记录
+                case = TestCase.objects.get(id=case_id)
+                # 获取页面上表单字段更新
+                case.name = req_name
+                case.module = module_obj
+                case.url = req_url
+                case.req_method = req_method
+                case.req_type = req_type
+                case.req_header = req_headers
+                case.req_parameter = req_parameter
+                case.response_assert = req_assert
+                case.save()
+                return HttpResponse("更新成功！")
+            else:
+                testcase = TestCase.objects.create(name=req_name, module=module_obj, url=req_url,
+                                           req_method=req_method, req_header=req_headers,
+                                           req_type=req_type, req_parameter=req_parameter, response_assert=req_assert)
+                if testcase is not None:
+                    return HttpResponse("保存成功！")
         else:
             return HttpResponse("请检查URL地址")
     else:
         return HttpResponse("请求方法错误")
 
-# 编辑测试用例
 @login_required
 def edit_case(request, case_id):
-    # print("编辑测试用例id：%d" %case_id)
-    # print("测试方法："+ request.method)
-    if request.method == "POST":
-        form = TestCaseForm(request.POST)
-        if form.is_valid():
-            # 查找编辑的用例记录
-            case = TestCase.objects.get(id=case_id)
-            # 获取页面上表单字段
-            case.name = form.cleaned_data['name']
-            case.module = form.cleaned_data['module']
-            case.url = form.cleaned_data['url']
-            case.req_method = form.cleaned_data['req_method']
-            case.req_type = form.cleaned_data['req_type']
-            case.req_header = form.cleaned_data['req_header']
-            case.req_parameter = form.cleaned_data['req_parameter']
-            case.response_assert = form.cleaned_data['response_assert']
-            # 获取参数进行判断
-            if case.url == "" or case.req_method == "" or case.req_type == "" or case.module == "":
-                return HttpResponse("必传参数为空")
-            if case.req_parameter == "":
-                case.req_parameter = "{}"
-            if case.req_header == "":
-                case.req_header = "{}"
-            case.save()
-            return HttpResponseRedirect('/interface/case_manage/')
+    """
+    编辑测试用例
+    :param request:
+    :param case_id:
+    :return:
+    """
+    if request.method == "GET":
+        projects = Project.objects.all()
+        return render(request, "interface_app/case_manage.html", {'type': 'edit', 'project_list': projects})
     else:
-        testcase = TestCase.objects.get(id=case_id)
-        form = TestCaseForm(instance=testcase)
-    return render(request, "interface_app/case_manage.html", {'type': 'edit', 'form': form})
+        return HttpResponse("404")
 
-# 删除测试用例
 @login_required
 def delete_case(request, case_id):
+    """
+    删除测试用例
+    :param request:
+    :param case_id:
+    :return:
+    """
     testcase = TestCase.objects.get(id=case_id)
     testcase.delete()
     return HttpResponseRedirect('/interface/case_manage/')
 
-# 验证url地址是否合法
 def url_validate(url):
+    """
+    验证url地址是否合法
+    :param url:
+    :return:
+    """
     flag = False
     regex = re.compile(
         r'^(?:http|ftp)s?://'  # http:// or https://
@@ -193,3 +229,62 @@ def url_validate(url):
     if re.match(regex, url):
         flag = True
     return flag
+
+def get_case_info(request):
+    """
+    获取测试用例记录
+    :param request:
+    :return:
+    """
+    case_id = request.GET.get("case_id")
+    # print("case_id", case_id)
+    if request.method == "GET":
+        if case_id =="":
+            return JsonResponse({"success": "false", "message": "case id is Null" })
+        case_obj = TestCase.objects.get(pk=case_id)
+        # 拿到模块id
+        module_id = case_obj.module_id
+        # print("模块id：", module_id)
+
+        # 通过模块id找到项目id
+        module = Module.objects.get(pk=module_id)
+        module_name = module.name
+
+        project_id = module.project_id
+        print("项目id：", project_id)
+
+        # 将用例信息返回
+        case_info ={
+            "name": case_obj.name,
+            "url": case_obj.url,
+            "req_method": case_obj.req_method,
+            "req_header": case_obj.req_header,
+            "req_type": case_obj.req_type,
+            "req_parameter": case_obj.req_parameter,
+            "module_id": module_id,
+            "project_id": project_id,
+            'response_assert': case_obj.response_assert
+        }
+        return JsonResponse({"success": "true", "message": "OK", "data": case_info})
+    else:
+        return HttpResponse("404")
+
+def api_assert(request):
+    """
+    验证测试用例断言
+    :param request:
+    :return:
+    """
+    if request.method == "POST":
+        result_text = request.POST.get("result_text")
+        assert_text = request.POST.get("assert_text")
+        if result_text == "" or assert_text == "":
+            return JsonResponse({"success": "false", "message": "验证结果和返回结果不能为空！"})
+        try:
+            assert assert_text in result_text
+        except AssertionError:
+            return JsonResponse({"success": "false", "message": "验证失败！"})
+        else:
+            return JsonResponse({"success": "true", "message": "验证成功！"})
+    else:
+        return JsonResponse({"success": "false", "message": "请求方法有误！"})
